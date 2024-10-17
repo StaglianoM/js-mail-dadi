f2856e9956f895824cdd817294ee23041ffa48c2
StaglianoM <stagliano.m92@gmail.com>
2024-10-17 16:55:44 +0200

    add "includes" & script html 2°file js

---
+++ email.js
@@ -0,0 +1,24 @@
+// se l'email è nella lista
+// stampo positivo
+
+// altrimenti 
+// stampo negativo 
+
+
+//  const email = [
+//      "rosso@live.it",
+//      "bianco@live.it",
+//      "giallo@live.it",
+//      "nero@live.it",
+//      "bianco@live.it"
+//    ];
+//    const emailUser = prompt("Inserire Email").toLowerCase(); 
+//    //.tolowercase il prompt viene scritto in minuscolo a prescindere
+
+//    if  (email.includes(emailUser)) {
+//    console.log(emailUser + " sei nella lista");
+//  } else {
+//    console.log(emailUser + " non è nella lista");
+//  }
+
+
