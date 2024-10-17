396e69b2117a911dbb56b3e1af907269234b0797
StaglianoM <stagliano.m92@gmail.com>
2024-10-17 17:18:35 +0200

    add true-false

--- email.js
+++ email.js
@@ -21,4 +21,42 @@
 //    console.log(emailUser + " non è nella lista");
 //  }
 
+        const email = [
+          "rosso@live.it",
+          "bianco@live.it",
+          "giallo@live.it",
+          "nero@live.it",
+        ];
+        const emailUser = prompt("Inserire Email").toLowerCase(); 
+        let emailFound = false; 
 
+// Se campo email è vuoto 
+// stampo messaggio nessuna email inserita
+// altrimenti continuo il ciclo
+
+        if (emailUser === "") {
+            alert("Nessuna email inserita.");  
+          } else {
+            let emailFound = false; 
+
+
+//ciclo for per cercare ogni step l'email se è presente o meno 
+//se email è uguale a email inserita dell'user - emailfound = vero 
+//quindi stampo
+//altrimenti è negativo quindi non è nella lista
+
+            for (let i = 0; i < email.length; i++) {
+            if (email[i].toLowerCase() === emailUser) {
+              emailFound = true;
+            }
+          }
+          
+//continuo ciclo SE email trovata (emailFound risultato true)
+//altrimenti email non trovata
+
+          if (emailFound) {
+            console.log(`Sei nella lista, ${emailUser} puoi accedere!`);
+          } else {
+            console.log(`Spiacente ${emailUser}, la tua email non è nella lista.`);
+          }
+        }
